66ba226c5e95f10cb469a1723a36f6edf7f582e4
Olivier Diotte <olivier@diotte.ca>
2017-10-18 19:01:50 -0400

    Measure I/O switch time

--- temp.py
+++ temp.py
@@ -8,16 +8,39 @@ mypin = 8
 #See image at the bottom of https://www.raspberrypi.org/documentation/usage/gpio/
 #for pin numbering
 GPIO.setmode(GPIO.BOARD)
+
 GPIO.setup(mypin, GPIO.OUT)
 
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-in10 = GPIO.input(10)
-in12 = GPIO.input(12)
-print('Pin 10 was {:d} (should be LOW), pin 12 was {:d} (should be HIGH)'.format(in10, in12))
+#Reset pulse
+t0 = time.time()
+GPIO.output(mypin, GPIO.LOW)
+time.sleep(0.0005) #must keep low at least 480us
+t0delta = time.time() - t0
 
-while (True):
-	GPIO.output(mypin, GPIO.HIGH)
-	time.sleep(0.5)
-	GPIO.output(mypin, GPIO.LOW)
-	time.sleep(0.5)
+#Read presence pulse
+t1 = time.time()
+GPIO.setup(mypin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+sleeptime = max(0, 0.00006 - (time.time() - t1))
+time.sleep(sleeptime) #Must wait at least 60us
+t1delta = time.time() - t1
+
+starttime = time.time()
+delta = None
+endtime = time.time() + 0.000250 #Bus is held low for 60us to 240us
+b_responded = False
+while (not b_responded and time.time() < endtime):
+	if (not GPIO.input(mypin)):
+		b_responded = True
+	if (delta is None):
+		delta = time.time() - starttime
+
+if (b_responded):
+	print('Sensor responded')
+else:
+	print('Sensor didn\'t respond')
+
+GPIO.cleanup()
+
+print('delta = {:f}'.format(delta))
+print('t0delta = {:f}'.format(t0delta))
+print('t1delta = {:f}'.format(t1delta))
