9c4198b596177c4de76b218d7e6b85ce57916aed
Olivier Diotte <olivier@diotte.ca>
2017-10-18 18:00:08 -0400

    Read GND and VCC on pins 10 and 12

--- temp.py
+++ temp.py
@@ -10,6 +10,12 @@ mypin = 8
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(mypin, GPIO.OUT)
 
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+in10 = GPIO.input(10)
+in12 = GPIO.input(12)
+print('Pin 10 was {:d} (should be LOW), pin 12 was {:d} (should be HIGH)'.format(in10, in12))
+
 while (True):
 	GPIO.output(mypin, GPIO.HIGH)
 	time.sleep(0.5)
